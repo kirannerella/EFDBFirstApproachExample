ff6c0d1062d4b0f2f4d16a6710bef94dec8a6ce2
kiran <kirann471@gmail.com>
2020-06-23 15:49:18 +0530

    Add project files.

---
+++ EFDbFirstApproachExample/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using EFDbFirstApproachExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDbFirstApproachExample.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        
+
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Category> catergories = db.Categories.ToList();
+            return View(catergories);
+        }
+    }
+}
